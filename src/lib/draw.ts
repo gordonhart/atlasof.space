dc7818f8f9292734235ea07ab05a7f5ca597e091
Gordon Hart <gordon.hart2@gmail.com>
2024-12-14 10:21:08 -0500

    Use simulated orbits

--- src/lib/draw.ts
+++ src/lib/draw.ts
@@ -1,7 +1,9 @@
 import { CelestialBodyState } from './types.ts';
 import { AppState } from './state.ts';
 import { findCelestialBody } from './constants.ts';
-import { degreesToRadians, semiMinorAxis } from './physics.ts';
+import { incrementState, orbitalPeriod } from './physics.ts';
+
+const ORBIT_CACHE: Record<string, Array<CelestialBodyState>> = {};
 
 export function drawBodies(ctx: CanvasRenderingContext2D, appState: AppState, systemState: CelestialBodyState) {
   const {
@@ -18,34 +20,35 @@ export function drawBodies(ctx: CanvasRenderingContext2D, appState: AppState, sy
 
   const dpr = window.devicePixelRatio ?? 1;
   const [canvasWidthPx, canvasHeightPx] = [ctx.canvas.width / dpr, ctx.canvas.height / dpr];
-  const [centerOffsetXm, centerOffsetYm] = findCelestialBody(systemState, center)?.position ?? [0, 0];
+  const centerBody = findCelestialBody(systemState, center);
+  const [centerOffsetXm, centerOffsetYm] = centerBody?.position ?? [0, 0];
   const [offsetXm, offsetYm] = [panOffsetXm - centerOffsetXm, panOffsetYm - centerOffsetYm];
 
-  const hoverBody = hover != null ? findCelestialBody(systemState, hover) : undefined;
-  if (hoverBody != null) {
-    const { semiMajorAxis, eccentricity, argumentOfPeriapsis } = hoverBody;
-    const radiusX = semiMajorAxis / metersPerPx;
-    const radiusY = semiMinorAxis(semiMajorAxis, eccentricity) / metersPerPx;
-    const rotation = degreesToRadians(argumentOfPeriapsis);
-    const centerX = canvasWidthPx / 2 + offsetXm / metersPerPx;
-    const centerY = canvasHeightPx / 2 + offsetYm / metersPerPx;
-    ctx.beginPath();
-    ctx.ellipse(centerX, centerY, radiusX, radiusY, Math.PI - rotation, 0, Math.PI * 2);
-    ctx.strokeStyle = hoverBody.color;
-    ctx.lineWidth = 1;
-    ctx.stroke();
-  }
-
-  function drawBody({ position, radius, color, satellites }: CelestialBodyState) {
+  function drawBody({ name, position, radius, color, satellites }: CelestialBodyState) {
     satellites.forEach(drawBody);
     const [positionXm, positionYm] = [position[0] + offsetXm, position[1] + offsetYm];
     const positionXpx = canvasWidthPx / 2 + positionXm / metersPerPx;
     const positionYpx = canvasHeightPx / 2 + positionYm / metersPerPx;
-    const radiusPx = (radius / metersPerPx) * planetScaleFactor;
+    const radiusPx = Math.max((radius / metersPerPx) * planetScaleFactor, 1);
+    const radiusScaledPx = name === hover ? radiusPx * 5 : radiusPx;
     ctx.beginPath();
-    ctx.arc(positionXpx, positionYpx, Math.max(radiusPx, 1), 0, Math.PI * 2);
+    ctx.arc(positionXpx, positionYpx, radiusScaledPx, 0, Math.PI * 2);
     ctx.fillStyle = color;
     ctx.fill();
+  }
+
+  const hoverBody = hover != null ? findCelestialBody(systemState, hover) : undefined;
+  if (hoverBody != null) {
+    if (ORBIT_CACHE[hoverBody.name] == null) {
+      const simplifiedSystem = { ...systemState, satellites: [{ ...hoverBody, name: 'foobar', satellites: [] }] };
+      const period = orbitalPeriod(hoverBody.semiMajorAxis, systemState.mass);
+      const targetSteps = 1000;
+      const dt = period / targetSteps;
+      ORBIT_CACHE[hoverBody.name] = Array(targetSteps)
+        .fill(null)
+        .reduce(acc => [...acc, incrementState(acc[acc.length - 1], dt)], [simplifiedSystem]);
+    }
+    ORBIT_CACHE[hoverBody.name].forEach(drawBody);
   }
 
   drawBody(systemState);
