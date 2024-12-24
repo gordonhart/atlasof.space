9799ab39ddb589ce391d31dcb01669e8c6d03b84
Gordon Hart <gordon.hart2@gmail.com>
2024-12-23 22:45:03 -0500

    Use a Tabler icon instead of custom SVG

--- src/components/Controls/DirectionIndicator.tsx
+++ src/components/Controls/DirectionIndicator.tsx
@@ -1,40 +1,38 @@
 import { AppState } from '../../lib/state.ts';
-import { buttonGap } from './constants.ts';
+import { buttonGap, iconSize } from './constants.ts';
 import { Group, Text, Tooltip } from '@mantine/core';
-import { IconZodiacAries } from '@tabler/icons-react';
+import { IconArrowRightBar, IconZodiacAries } from '@tabler/icons-react';
 
 type Props = Pick<AppState, 'vernalEquinox'>;
 export function DirectionIndicator({ vernalEquinox }: Props) {
   const angle = Number(Math.atan2(vernalEquinox[1], vernalEquinox[0]).toFixed(3));
+  const pad = 4;
+  const width = iconSize + pad * 2;
   return (
     <Tooltip
       label={
         <Group gap={4}>
           <Text inherit>Vernal Equinox</Text>
-          <IconZodiacAries size={14} />
+          <IconZodiacAries size={iconSize} />
         </Group>
       }
       position="left"
     >
       <Group
-        p={8}
-        w={30}
-        h={30}
+        p={pad}
+        w={width}
+        h={width}
         gap={buttonGap}
         justify="center"
         align="center"
         style={{
-          borderRadius: 15,
+          borderRadius: width / 2,
           backdropFilter: 'blur(4px)',
           transform: `rotate(${angle}rad)`,
           transformOrigin: 'center',
         }}
       >
-        <svg style={{ width: 14 }} xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 284.14 240.7">
-          <g style={{ fill: 'var(--mantine-color-gray-light-color)' }}>
-            <polygon points="283.34 120.52 284.14 120.52 283.74 120.35 284.14 120.18 283.34 120.18 0 0 104.18 120.35 0 240.7 283.34 120.52" />
-          </g>
-        </svg>
+        <IconArrowRightBar size={iconSize} />
       </Group>
     </Tooltip>
   );
