13a0b8e0ee70ea3e8735f0be8fd54b0b24e417f4
Gordon Hart <gordon.hart2@gmail.com>
2025-01-03 17:52:39 -0500

    Fix build

--- src/components/Controls/GeneralControls.tsx
+++ src/components/Controls/GeneralControls.tsx
@@ -2,7 +2,6 @@ import { ActionIcon, Group, Tooltip } from '@mantine/core';
 import { IconCircle, IconCircleDot, IconRestore, IconTagMinus, IconTagPlus } from '@tabler/icons-react';
 import { AppStateControlProps, buttonGap, iconSize } from './constants.ts';
 import { memo } from 'react';
-import { AddSmallBodyModal } from './AddSmallBodyModal.tsx';
 import { SelectOmnibox } from './SelectOmnibox.tsx';
 import { HelpModalButton } from './HelpModalButton.tsx';
 import { VisibilityControls } from './VisibilityControls.tsx';
@@ -10,26 +9,16 @@ import { VisibilityControls } from './VisibilityControls.tsx';
 type Props = AppStateControlProps & {
   reset: () => void;
 };
-export const GeneralControls = memo(function GeneralControlsComponent({
-  state,
-  updateState,
-  addBody,
-  removeBody,
-  reset,
-}: Props) {
+export const GeneralControls = memo(function GeneralControlsComponent({ state, updateState, reset }: Props) {
   return (
     <Group gap={buttonGap}>
-      {/* TODO: these two controls should be merged, ideally */}
       <SelectOmnibox state={state} updateState={updateState} />
-      <AddSmallBodyModal state={state} addBody={addBody} removeBody={removeBody} />
 
       <Tooltip position="top" label={`${state.drawOrbit ? 'Hide' : 'Show'} Orbits`}>
         <ActionIcon onClick={() => updateState({ drawOrbit: !state.drawOrbit })}>
           {state.drawOrbit ? <IconCircleDot size={iconSize} /> : <IconCircle size={iconSize} />}
         </ActionIcon>
       </Tooltip>
-
-      <VisibilityControls state={state} updateState={updateState} />
 
       <Tooltip position="top" label={`${state.drawLabel ? 'Hide' : 'Show'} Labels`}>
         <ActionIcon onClick={() => updateState({ drawLabel: !state.drawLabel })}>
@@ -44,6 +33,8 @@ export const GeneralControls = memo(function GeneralControlsComponent({
         </ActionIcon>
       </Tooltip>
       */}
+
+      <VisibilityControls state={state} updateState={updateState} />
 
       <Tooltip position="top" label="Reset">
         <ActionIcon onClick={reset}>
