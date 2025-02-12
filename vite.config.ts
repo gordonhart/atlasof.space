f6a3e1e85e0de5a08cd4cfe0651e3a5a46923815
Gordon Hart <gordon.hart2@gmail.com>
2025-02-12 08:53:28 -0500

    Split vendor chunk into separate file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   assetsInclude: ['assets/*.{png,gif,jpg,ttf}'],
 });
