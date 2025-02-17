a7fc4b5d6570e05db9b3b652d010cb710966b3cd
Gordon Hart <gordon.hart2@gmail.com>
2025-02-17 09:53:53 -0500

    Update CelestialBodyFactSheet props to better memoize

--- src/components/FactSheet/HillSphereToggle.tsx
+++ src/components/FactSheet/HillSphereToggle.tsx
@@ -1,16 +1,16 @@
 import { Group, Switch } from '@mantine/core';
 import { hillRadius } from '../../lib/physics.ts';
-import { Settings, UpdateSettings } from '../../lib/state.ts';
+import { ToggleId, UpdateSettings } from '../../lib/state.ts';
 import { asHillSphereId, CelestialBody } from '../../lib/types.ts';
 import { humanDistanceUnits } from '../../lib/utils.ts';
 
 type Props = {
   body: CelestialBody;
   parent: CelestialBody;
-  settings: Settings;
+  toggles: Set<ToggleId>;
   updateSettings: UpdateSettings;
 };
-export function HillSphereToggle({ body, parent, settings, updateSettings }: Props) {
+export function HillSphereToggle({ body, parent, toggles, updateSettings }: Props) {
   const { semiMajorAxis: a, eccentricity: e } = body.elements;
   const hillRad = hillRadius(a, e, parent.mass, body.mass);
   const [radValue, radUnits] = humanDistanceUnits(hillRad);
@@ -23,7 +23,7 @@ export function HillSphereToggle({ body, parent, settings, updateSettings }: Pro
     });
   }
 
-  const isActive = settings.toggles.has(id);
+  const isActive = toggles.has(id);
   return (
     <Group gap={8} wrap="nowrap" fw="normal" fz="xs" c={isActive ? undefined : 'var(--mantine-color-gray-light-color)'}>
       {(radValue > 1e4 ? Number(radValue.toFixed(0)) : radValue).toLocaleString()} {radUnits}
